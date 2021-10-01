7719138b0880ff0e2a1c8040e65bf2216811ebb5
zouzouzou11 <1096174991@qq.com>
2021-10-02 01:13:56 +0800

    factory method :log system example add

--- src/creational/FactoryMethod.ts
+++ src/creational/FactoryMethod.ts
@@ -35,11 +35,58 @@ class ConcreteFactoryCreator implements FactoryCreator {
   }
 }
 
+/**
+ * 举个实际例子：任何系统都有可能用到的日志记录器
+ */
+abstract class LogSystem<T> {
+  abstract logs: T[];
+  abstract writeLog(log: T): void;
+}
+
+class ProgressLogSystem implements LogSystem<number> {
+  logs: number[] = [];
+  writeLog(log: number) {
+    this.logs.push(log);
+    return;
+  }
+}
+
+class FileLogSystem implements LogSystem<string> {
+  logs: string[] = [];
+  writeLog(log: string) {
+    this.logs.push(log);
+    return;
+  }
+}
+
+abstract class LogSystemFactory {
+  abstract createLogSystem(): LogSystem<unknown>;
+}
+
+class FileLogSystemFactory implements LogSystemFactory {
+  createLogSystem() {
+    return new FileLogSystem();
+  }
+}
+
+class ProgressLogSystemFactory implements LogSystemFactory {
+  createLogSystem() {
+    return new ProgressLogSystem();
+  }
+}
+
 export function test() {
   console.warn("factory method");
   console.log(
     "ConcreteFactoryCreator:",
     new ConcreteFactoryCreator().factorMethod().getName()
   );
+  const progressLogSystemFactory = new ProgressLogSystemFactory();
+  const progressLogSystem = progressLogSystemFactory.createLogSystem();
+  progressLogSystem.writeLog(9);
+  const fileLogSystemFactory = new FileLogSystemFactory();
+  const fileLogSystem = fileLogSystemFactory.createLogSystem();
+  fileLogSystem.writeLog("file log system");
+  console.log("logSystems:", progressLogSystem.logs, fileLogSystem.logs);
   console.log("---");
 }
