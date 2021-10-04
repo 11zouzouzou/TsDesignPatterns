5abbe82a7cc283aaa1198c226e6d18befcd91c7f
zouzouzou11 <1096174991@qq.com>
2021-10-04 20:28:29 +0800

    composite example add

--- src/structural/Composite.ts
+++ src/structural/Composite.ts
@@ -35,11 +35,63 @@ class Leaf extends Component {
   }
 }
 
+/**
+ * 举个例子：公司员工
+ */
+
+class Employee {
+  name: string;
+  dept: string;
+  salary: number;
+  subordinates: Employee[];
+  constructor(name: string, dept: string, salary: number) {
+    this.name = name;
+    this.dept = dept;
+    this.salary = salary;
+    this.subordinates = [];
+  }
+  add(e: Employee) {
+    this.subordinates.push(e);
+  }
+  getSubordinates(): Employee[] {
+    return this.subordinates;
+  }
+}
+
 export function test() {
   console.warn("composite pattern");
   const root = new Component(1)
     .add(new Composite(2).add(new Leaf(4)))
     .add(new Leaf(3));
   root.operation();
+  console.log("example:");
+  const CEO = new Employee("John", "CEO", 30000);
+  const headSales = new Employee("Robert", "Head Sales", 20000);
+  const headMarketing = new Employee("Michel", "Head Marketing", 20000);
+  const clerk1 = new Employee("Laura", "Marketing", 10000);
+  const clerk2 = new Employee("Bob", "Marketing", 10000);
+
+  const salesExecutive1 = new Employee("Richard", "Sales", 10000);
+  const salesExecutive2 = new Employee("Rob", "Sales", 10000);
+
+  CEO.add(headSales);
+  CEO.add(headMarketing);
+
+  headSales.add(salesExecutive1);
+  headSales.add(salesExecutive2);
+
+  headMarketing.add(clerk1);
+  headMarketing.add(clerk2);
+  //打印该组织的所有员工
+  console.log("打印所有员工");
+  console.log(CEO.name);
+  const arr = CEO.getSubordinates();
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i].name);
+    const next = arr[i].getSubordinates();
+    for (let i = 0; i < next.length; i++) {
+      console.log(next[i].name);
+    }
+  }
   console.log("---");
 }
