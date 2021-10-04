3a6acf186b898567439b2ecf1990920c3fc2225f
zouzouzou11 <1096174991@qq.com>
2021-10-04 18:55:34 +0800

    flyweight example add

--- src/structural/Flyweight.ts
+++ src/structural/Flyweight.ts
@@ -40,7 +40,9 @@ class ConcreteFlyweight implements Flyweight {
 class FlyweightFactory {
   private flyweights: { [key: string]: ConcreteFlyweight } = {}; //共享池
   add(extrinsic: any) {
-    this.flyweights[JSON.stringify(extrinsic)] = new ConcreteFlyweight(extrinsic);
+    this.flyweights[JSON.stringify(extrinsic)] = new ConcreteFlyweight(
+      extrinsic
+    );
     return this;
   }
   //参数getExistOnly为true只获得当前已经存在的，否则返回为undefined,用于验证缓存是否被启用
@@ -55,6 +57,52 @@ class FlyweightFactory {
   }
 }
 
+/**
+ * 举个例子：五子棋小游戏案例
+ */
+
+class Chess {
+  x: number = 0;
+  y: number = 0;
+  play(x: number, y: number) {}
+}
+
+class WhiteChess extends Chess {
+  play(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    return "white:" + "x:" + x + "y:" + y;
+  }
+}
+
+class BlackChess extends Chess {
+  play(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    return "black:" + "x:" + x + "y:" + y;
+  }
+}
+
+class ChessFactory {
+  private static whiteChess: Chess;
+  private static blackChess: Chess;
+  public static getChess(key: number): Chess {
+    if (key == 0) {
+      if (ChessFactory.whiteChess == null) {
+        console.log("=====创建白棋对象======");
+        ChessFactory.whiteChess = new WhiteChess();
+      }
+      return ChessFactory.whiteChess;
+    } else {
+      if (ChessFactory.blackChess == null) {
+        console.log("=====创建黑棋对象======");
+        ChessFactory.blackChess = new BlackChess();
+      }
+      return ChessFactory.blackChess;
+    }
+  }
+}
+
 export function test() {
   console.log("flyweight:");
   const flyWeightPool = new FlyweightFactory();
@@ -63,5 +111,11 @@ export function test() {
   console.log(flyWeightPool.getFlyweight({ zou: 1 }, true));
   console.log(flyWeightPool.getFlyweight({ zou: 3 }, true));
   console.log(flyWeightPool.getFlyweight({ zou: 3 }, false));
+  console.log("example:");
+  for (let i = 0; i < 10; i++) {
+    let c = ChessFactory.getChess(i % 2);
+    console.log(c.play(i, Math.round(Math.random() * 20)));
+  }
+
   console.log("---");
 }
